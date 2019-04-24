8366200567934af9f5dbe2f48ba62093339415f4
Wanglei <lei.wang@geneegroup.com>
2019-04-24 13:48:29 +0800

    去除无用的代码

--- src/app.js
+++ src/app.js
@@ -1,21 +1,8 @@
-// import '~public/style/css/element-theme/index.css'
 import '~public/style/index.styl'
 import Vue from 'vue'
 import axios from 'axios'
-// import { Input, Button, Select, OptionGroup, Option, RadioGroup, Radio, , Message, Cascader, DatePicker } from 'element-ui'
 import { Loading, Message } from 'gapper-element-ui'
 import { app } from '~/config'
-
-// Vue.prototype.$ELEMENT = { size: 'medium' }
-// Vue.use(Input)
-// Vue.use(Button)
-// Vue.use(Select)
-// Vue.use(OptionGroup)
-// Vue.use(Option)
-// Vue.use(RadioGroup)
-// Vue.use(Radio)
-// Vue.use(Cascader)
-// Vue.use(DatePicker)
 
 export function beforeCreateApp () {
   axios.defaults.timeout = 5000
